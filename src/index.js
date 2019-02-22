440c1e74290f6a3e806501457f5e0b705a3b7708
Alex Petrochenko <alexjustfly@gmail.com>
2019-02-22 13:19:30 +0300

    add answer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	let count = 0;
+	let arr = preferences;
+
+	for (let i = 0; i < arr.length; i++) {
+		var k = arr[i];
+		var n = i+1;
+		var m = arr[k-1];
+		if (arr[m-1]==n && m!=n) {count+=1;}
+	}
+	count/=3;
+	return count;
 };
